2cca8ac66645d6dd92b6f4c1403cdf89cbf163af
mkhamdank <gsyn153@gmail.com>
2022-05-12 09:53:31 +0700

    vaksin fix

--- resources/views/human_resources/vaksin/index.blade.php
+++ resources/views/human_resources/vaksin/index.blade.php
@@ -448,7 +448,13 @@
                         $('#vaksin_register').hide();
                         $('#vaksin_register_btn_cancel').hide();
                         $('#call_vaksin_3_register').val('');
+                    }else{
+                        $('#vaksin_register_btn').hide();
+                        $('#vaksin_register').hide();
+                        $('#vaksin_register_btn_cancel').hide();
+                        $('#call_vaksin_3_register').val('');
                     }
+
                 }else {
                     $('#vaksin_1').val(result.vaksin.vaksin_1);
                     $('#vaksin_2').val(result.vaksin.vaksin_2);
