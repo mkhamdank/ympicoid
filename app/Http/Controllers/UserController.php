932d29418c7381b361f0a5cf69cc0f2bc0c105c1
mkhamdank <gsyn153@gmail.com>
2022-07-11 11:51:42 +0700

    db

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 use App\User;
 use Illuminate\Database\QueryException;
 use App\Role;
