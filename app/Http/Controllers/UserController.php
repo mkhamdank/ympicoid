130868a4438f753d69ce20668c040203a448e35c
mkhamdank <gsyn153@gmail.com>
2022-07-11 11:53:00 +0700

    wuer

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 use App\User;
 use Illuminate\Database\QueryException;
